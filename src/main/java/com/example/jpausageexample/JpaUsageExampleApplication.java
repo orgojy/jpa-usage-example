e688cf08a958274f8b55361e4525dfdb182f616a
orgojy <giveawesome@gmail.com>
2021-02-09 21:30:49 +0900

    feat: initiation settings for project

---
+++ src/main/java/com/example/jpausageexample/JpaUsageExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpausageexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUsageExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUsageExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpausageexample/JpaUsageExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpausageexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaUsageExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
