503c7380f88f6cdca9213d3559513f9756ddddae
orgojy <giveawesome@gmail.com>
2021-02-12 17:21:40 +0900

    feat: domain for example "Product"

---
+++ src/main/java/com/example/jpausageexample/domain/Product.java
@@ -0,0 +1,42 @@
+package com.example.jpausageexample.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    protected Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
