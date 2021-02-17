5b0abfe8d8d61bf46800f9c9d7ccf50e8c311256
orgojy <giveawesome@gmail.com>
2021-02-17 22:20:40 +0900

    test: unit test to persist entity using by entity manager

--- src/test/java/com/example/jpausageexample/domain/ProductTest.java
+++ src/test/java/com/example/jpausageexample/domain/ProductTest.java
@@ -3,6 +3,7 @@ package com.example.jpausageexample.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -13,7 +14,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class ProductTest {
     @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
     private ProductRepository productRepository;
+
+    @Test
+    void persistTestWithEntityManager() {
+        // given
+        Product product = new Product("Apple", BigDecimal.valueOf(1_000));
+        product = entityManager.persist(product);
+
+        // when
+        Product savedProduct = entityManager.find(Product.class, product.getId());
+
+        // then
+        assertThat(savedProduct).isEqualTo(product);
+    }
 
     @Test
     void saveTestWithoutTransaction() {
