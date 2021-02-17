f7043615217fa0a47201a8955d9e8857bd7e52dc
orgojy <giveawesome@gmail.com>
2021-02-17 22:26:49 +0900

    chore: unused imports class

--- src/test/java/com/example/jpausageexample/domain/ProductTest.java
+++ src/test/java/com/example/jpausageexample/domain/ProductTest.java
@@ -3,7 +3,6 @@ package com.example.jpausageexample.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
